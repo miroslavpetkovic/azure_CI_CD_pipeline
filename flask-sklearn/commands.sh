7b40cfb0c6707251958d9171b27a7f353a956ed3
Miroslav Petkovic <79813653+miroslavpetkovic@users.noreply.github.com>
2021-04-04 00:35:18 +0200

    Add files via upload

--- flask-sklearn/commands.sh
+++ flask-sklearn/commands.sh
@@ -1,1 +1,8 @@
-az webapp up --sku F1 --name flask-ml-lingchenzhu --location "East US"
+git clone git@github.com:PaulNWms/udacity-cicd-pipeline.git
+python3 -m venv ~/cicd
+source ~/cicd/bin/activate
+cd udacity-cicd-pipeline
+make all
+export FLASK_APP=app.py
+az webapp up --sku F1 -n udacity-flask-ml-service
+
